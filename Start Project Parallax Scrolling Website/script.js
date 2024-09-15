541a8134fbf1eeb14cc1f5e5fe20a43499f6cc6e
SreekuttanCS <sreekuttancs26@gmail.com>
2024-09-15 15:40:06 +0530

    updates

--- Start Project Parallax Scrolling Website/script.js
+++
@@ -1,5 +0,0 @@
-let text = document.getElementById('text');
-let treeLeft = document.getElementById('tree-left');
-let treeRight = document.getElementById('tree-right');
-let gateLeft = document.getElementById('gate-left');
-let gateRight = document.getElementById('gate-right');
